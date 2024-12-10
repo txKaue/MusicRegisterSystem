ce32a336ca6427dec31ecacc6c71aaa4cfa0c162
Kaue Teixeira <kauetxp@gmail.com>
2024-12-09 21:43:38 -0300

    Busca de dados pronta

--- dataBase/data.py
+++ dataBase/data.py
@@ -18,8 +18,20 @@ class Data:
             print("Erro ao adicionar ao banco de dados.")
 
     def GetDados(self, colection):
-        data = self.collection(colection).get()
-        return data
+        data = self.db.collection(colection).get()
+        result = []
+
+        for doc in data:
+            doc_dict = doc.to_dict()
+            reordered_dict = {
+                "Nome": doc_dict["Nome"],
+                "Artista": doc_dict["Artista"],
+                "Genero": doc_dict["Genero"],
+            }
+            
+            result.append(reordered_dict)
+        
+        return result
     
     def MostrarDados(self, colection):
         data = self.db.collection(colection).get()
@@ -31,8 +43,10 @@ class Data:
             original = self.db.collection(colection).document(id)
             original.update(data)
             print(f"Item alterado com sucesso.")
-        except:
-            print(f"Não foi possível editar o item {id}")
+            return True
+        except Exception as e:
+            print(f"Erro ao editar o item {id}: {e}")
+            return
 
     def getIdByName(self, colection, nome):
         items = self.db.collection(colection).where(field_path="Nome", op_string="==", value=nome).get()
@@ -41,6 +55,19 @@ class Data:
             return items[0].id
         else:
             print("Item não encontrado")
+            return None
+        
+    def getItemById(self, colection, item_id):
+        try:
+            document = self.db.collection(colection).document(item_id).get()
+            if document.exists:
+                return document.to_dict()
+            else:
+                print("Documento não encontrado")
+                return None
+            
+        except Exception as e:
+            print(f"Erro ao buscar o documento: {e}")
             return None
             
     def DeleteDados(self, colection, id):
--- interface/interface.py
+++ interface/interface.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
 from dataBase.data import Data
 
 class Interface:
@@ -11,13 +11,14 @@ class Interface:
         self.artistaInput = ""
         self.generoInput = ""
         self.root.title("Cadastro de músicas") #Mudando o nome da janela
-        self.width = 600
-        self.height = 400
+        self.width = 720
+        self.height = 560
         centerPosition = self.getCentroDaTela()
-        self.root.geometry(f'600x400+{centerPosition[0]}+{centerPosition[1]}') #Definindo as medidas da janela
+        self.root.geometry(f'{self.width}x{self.height}+{centerPosition[0]}+{centerPosition[1]}') #Definindo as medidas da janela
         self.root.resizable(False, False) #Impede o user de mudar o tamanho da tela
         self.root.iconbitmap('./assets/images/logoApp.ico') #Mudando o icone da janela
-        self.MontarTela()
+        self.mudar = False
+        self.MontarAdicionar()
 
 
     def Abrir(self):
@@ -30,7 +31,7 @@ class Interface:
         center_y = int(screen_height/2 - self.height / 2)
         return [center_x, center_y]
     
-    def MontarTela(self):
+    def MontarAdicionar(self):
 
         #Primeiro vou dividir as áreas da tela
         lFrame = ttk.Frame(self.root, height=self.height, width=self.width/2)
@@ -56,13 +57,26 @@ class Interface:
         self.generoInput = ttk.Entry(lFrame, width=30)
         self.generoInput.pack(pady=0, padx=10, anchor='nw')
 
-        ttk.Button(lFrame, text="Adicionar", command=self.Enviar).pack()
+        self.botaoAdicionarMudar = ttk.Button(lFrame, text="Adicionar", command=self.Enviar)
+        self.botaoAdicionarMudar.pack() 
+
+        #Montando o lado de busca no banco de dados
+        ttk.Label(rFrame, text='Pesquisar por nome', font=("Arial", 10)).pack(pady=10, padx=10, anchor='nw')
+        self.buscaInput = ttk.Entry(rFrame, width=45)
+        self.buscaInput.pack(pady=10, padx=10, anchor='nw')
+
+        ttk.Button(rFrame, text="Buscar", command=self.Buscar).pack(pady=20)
+
+        #Montando a visualização de alguns dados:
+        self.listbox = tk.Listbox(rFrame, width=55, height=5, font=("Arial", 8))  # Definindo a altura para 5 itens visíveis
+        self.listbox.pack(pady=10, padx= 10, anchor="nw")
+        self.AtualizarLista()
 
     def SerializarInput(self, nome, artista, genero):
         data = {}
         data["Nome"] = nome
         data["Artista"] = artista
-        data["Gênero"] = genero
+        data["Genero"] = genero
         return data
     
     def LimparCampos(self):
@@ -78,3 +92,51 @@ class Interface:
         colection = "Músicas"
         self.db.AdicionarDados(colection, data)
         self.LimparCampos()
+        self.AtualizarLista()
+
+    def PreencherDados(self, data):
+        self.nomeInput.insert(0, data["Nome"])
+        self.artistaInput.insert(0, data["Artista"])
+        self.generoInput.insert(0, data["Genero"])
+
+    def Buscar(self):
+        busca = self.buscaInput.get()
+        data = self.db.getItemById("Músicas", self.db.getIdByName("Músicas", busca))
+        if data != None:
+            self.botaoAdicionarMudar.config(text="Alterar", command=self.Alterar)
+            self.PreencherDados(data)
+        else:
+            messagebox.showerror("Erro", "Item não encontrado no banco.")
+
+    def Alterar(self):
+        nome = self.nomeInput.get()
+        artista = self.artistaInput.get()
+        genero = str(self.generoInput.get())
+
+        if nome == None or artista == None or genero == None:
+            messagebox.showerror("Erro", "Campos vazios operação cancelada.")
+            self.botaoAdicionarMudar.config(text="Adicionar", command=self.Enviar)
+            self.LimparCampos()
+        else:
+            id = self.db.getIdByName("Músicas", nome)
+            data = self.SerializarInput(nome, artista, genero)
+            print(data)
+            sucesso = self.db.AlterarDados("Músicas", id, data)
+            if sucesso:
+                self.botaoAdicionarMudar.config(text="Adicionar", command=self.Enviar)
+                self.LimparCampos()
+                self.AtualizarLista()
+            else:
+                messagebox.showerror("Erro", "Inserção de dados inválida.")
+                self.botaoAdicionarMudar.config(text="Adicionar", command=self.Enviar)
+                self.LimparCampos()
+
+    def AtualizarLista(self):
+        self.listbox.delete(0, tk.END)
+
+        data = self.db.GetDados("Músicas")
+        for i in range(len(data)):
+            if i == 5:
+                break
+            else:
+                self.listbox.insert(tk.END, data[i])
